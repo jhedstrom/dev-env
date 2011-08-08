546c5f1fee2082694efd8fc4d9f3e343bd82e56f
Jonathan Hedstrom <jhedstrom@gmail.com>
2011-08-08 15:57:55 -0700

    Adding gargravar aliases and others.

--- .drush/aliases.drushrc.php
+++ .drush/aliases.drushrc.php
@@ -2,6 +2,28 @@
 
 $aliases['wef'] = array(
   'uri' => 'default',
-  'root' => '/home/jonathan/work/clients/acquia-wef/docroot',
+  'root' => '/home/jhedstrom/work/clients/acquia-wef/docroot',
 );
 
+$aliases['dr'] = array(
+  'uri' => 'dr.dev',
+  'root' => '/home/jhedstrom/work/drupal',
+);
+
+$aliases['osu'] = array(
+  'uri' => 'default',
+  'root' => '/home/jhedstrom/work/clients/oregon_state_university/app/drupal',
+);
+
+$aliases['fc'] = array(
+  'uri' => 'default',
+  'root' => '/home/jhedstrom/work/clients/fluke/app/drupal',
+);
+
+$aliases['do'] = array(
+  'uri' => 'do.dev',
+  'root' => '/home/jhedstrom/work/drupal-6',
+);
+
+// Gargravarr aliases.
+require '/home/jhedstrom/work/opensourcery/drush.gargravarr/aliases.drushrc.php';
