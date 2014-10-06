6d5662b749eb7b39116662cfc0185c00a860ca12
Jonathan Hedstrom <jhedstrom@gmail.com>
2014-10-06 10:48:29 -0700

    Updating drush aliases.

--- .drush/aliases.drushrc.php
+++ .drush/aliases.drushrc.php
@@ -1,92 +1,5 @@
 <?php
 
-$aliases['ase'] = array(
-  'root' => '/home/jhedstrom/work/clients/ase/app/drupal',
-  'uri' => 'http://ase.dev',
-);
-
-$aliases['rp'] = array(
-  'root' => '/home/jhedstrom/work/clients/regional-programs/app/drupal',
-  'uri' => 'http://rp.dev',
-);
-
-$aliases['211'] = array(
-  'root' => '/home/jhedstrom/work/clients/211info/app/drupal',
-  'uri' => 'http://211.dev',
-);
-
-$aliases['hfp'] = array(
-  'root' => '/home/jhedstrom/work/clients/homeforward-public/app/drupal',
-  'uri' => 'http://hfp.dev',
-);
-
-$aliases['cm'] = array(
-  'root' => '/home/jhedstrom/work/clients/cascade-microtech/app/drupal',
-  'uri' => 'http://cm.dev',
-);
-
-$aliases['oo'] = array(
-  'root' => '/home/jhedstrom/work/drupal',
-  'uri' => 'http://oo.dev',
-);
-
-$aliases['s4rs'] = array(
-  'uri' => 'http://s4rs.dev',
-  'root' => '/home/jhedstrom/work/clients/solar4rschools/app/drupal',
-);
-
-$aliases['core'] = array(
-  'root' => '/home/jhedstrom/work/clients/CORE/app/drupal',
-);
-
-$aliases['cchc'] = array(
-  'root' => '/home/jhedstrom/work/clients/cchc/app/drupal',
-);
-
-$aliases['eap'] = array(
-  'root' => '/home/jhedstrom/work/clients/eap/docroot',
-);
-
-$aliases['pug'] = array(
-  'uri' => 'http://pug.dev',
-  'root' => '/home/jhedstrom/work/clients/princeton-university-giving/app/drupal',
-);
-
-$aliases['cf'] = array(
-  'uri' => 'default',
-  'root' => '/home/jhedstrom/work/clients/collins/app/drupal',
-);
-
-$aliases['crp'] = array(
-  'uri' => 'default',
-  'root' => '/home/jhedstrom/work/clients/crp/app/drupal',
-);
-
-$aliases['os6'] = array(
-  'uri' => 'default',
-  'root' => '/home/jhedstrom/work/PublicSite/docroot',
-);
-
-$aliases['shift'] = array(
-  'uri' => 'default',
-  'root' => '/home/jhedstrom/work/clients/shift/app/drupal',
-);
-
-$aliases['di'] = array(
-  'uri' => 'default',
-  'root' => '/home/jhedstrom/work/DrupalInit/app/drupal',
-);
-
-$aliases['zoo'] = array(
-  'uri' => 'default',
-  'root' => '/home/jhedstrom/work/clients/Oregon-Zoo/app/drupal',
-);
-
-$aliases['d8blocks'] = array(
-  'uri' => 'default',
-  'root' => '/home/jhedstrom/work/contributions/core/drupal_8_blocks_everywhere',
-);
-
 $aliases['d8'] = array(
   'uri' => 'default',
   'root' => '/home/jhedstrom/work/drupal-8',
@@ -97,89 +10,9 @@ $aliases['dd7'] = array(
   'root' => '/home/jhedstrom/work/dominiondeck/app/drupal',
 );
 
-$aliases['scf'] = array(
-  'uri' => 'default',
-  'root' => '/home/jhedstrom/work/clients/sierra-club-foundation/app/drupal',
-);
-
-$aliases['wef'] = array(
-  'uri' => 'default',
-  'root' => '/home/jhedstrom/work/clients/acquia-wef/docroot',
-);
-
-$aliases['dc'] = array(
-  'uri' => 'dc.dev',
-  'root' => '/home/jhedstrom/work/drupal-6',
-);
-
 $aliases['dd'] = array(
   'uri' => 'default',
   'root' => '/home/jhedstrom/work/dominiondeck/app/drupal',
-);
-
-$aliases['sr'] = array(
-  'uri' => 'default',
-  'root' => '/home/jhedstrom/work/clients/streetroots',
-);
-
-$aliases['ns'] = array(
-  'uri' => 'ns.dev',
-  'root' => '/home/jhedstrom/work/drupal',
-);
-
-$aliases['vr'] = array(
-  'uri' => 'vr.dev',
-  'root' => '/home/jhedstrom/work/drupal-6',
-);
-
-$aliases['vr7'] = array(
-  'uri' => 'vr7.dev',
-  'root' => '/home/jhedstrom/work/drupal',
-);
-
-$aliases['np'] = array(
-  'uri' => 'np.dev',
-  'root' => '/home/jhedstrom/work/drupal',
-);
-
-$aliases['ds'] = array(
-  'uri' => 'ds.dev',
-  'root' => '/home/jhedstrom/work/drupal',
-);
-
-$aliases['dr'] = array(
-  'uri' => 'dr.dev',
-  'root' => '/home/jhedstrom/work/drupal-6',
-);
-
-$aliases['osu'] = array(
-  'uri' => 'default',
-  'root' => '/home/jhedstrom/work/clients/osu_catalog/app/drupal',
-);
-
-$aliases['fc'] = array(
-  'uri' => 'default',
-  'root' => '/home/jhedstrom/work/clients/fluke/app/drupal',
-);
-
-$aliases['dassc'] = array(
-  'uri' => 'dassc.dev',
-  'root' => '/home/jhedstrom/work/drupalorg/association.drupal.org-7',
-);
-
-$aliases['do7'] = array(
-  'uri' => 'do7.dev',
-  'root' => '/home/jhedstrom/work/drupalorg/drupal.org-7',
-);
-
-$aliases['gdo'] = array(
-  'uri' => 'gdo.dev',
-  'root' => '/home/jhedstrom/work/drupalorg/groups.drupal.org',
-);
-
-$aliases['do'] = array(
-  'uri' => 'do.dev',
-  'root' => '/home/jhedstrom/work/drupalorg/drupal.org',
 );
 
 $aliases['d6'] = array(
@@ -192,8 +25,5 @@ $aliases['d7'] = array(
   'root' => '/home/jhedstrom/work/drupal'
 );
 
-// Gargravarr aliases.
-require '/home/jhedstrom/work/opensourcery/drush.gargravarr/aliases.drushrc.php';
-
 // Non-local
-require '/home/jhedstrom/.drush/private.aliases.php';
+require __DIR__ . '/private.aliases.php';
